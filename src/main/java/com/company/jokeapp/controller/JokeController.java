fb59b6e72a3d59ca0be7088ee1ef2e07acc60ed8
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-08-30 14:55:44 +0430

    Refactor JokeServiceImpl add bean to ChuckConfiguration

--- src/main/java/com/company/jokeapp/controller/JokeController.java
+++ src/main/java/com/company/jokeapp/controller/JokeController.java
@@ -1,8 +1,7 @@
-package com.company.jokeapp.Controller;
+package com.company.jokeapp.controller;
 
 import com.company.jokeapp.service.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
